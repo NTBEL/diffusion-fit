ade852c72ba81d63e6a2ffea2bf1a478b26a47be
Blake A. Wilson <blakeaw1102@gmail.com>
2022-01-13 15:41:50 -0600

    New measure module.

---
+++ diffusionfit/measure.py
@@ -0,0 +1,41 @@
+"""
+"""
+
+import numpy as np
+import numba
+
+
+@numba.njit(cache=True)
+def ss_error(observed_values, estimated_values):
+    """Sum of squared error function.
+    """
+    sse = np.sum((observed_values - estimated_values)**2)
+    return sse
+
+@numba.njit(cache=True)
+def sse_to_rmse(sse, N_values):
+    """Sum of squared error function.
+    """
+    rmse = np.sqrt(sse / N_values)
+    return rmse
+
+@numba.njit(cache=True)
+def rms_error(observed_values, estimated_values):
+    """Root-mean-squared Error function.
+    """
+    N_values = np.prod(observed_values.shape)
+    sse = ss_error(observed_values, estimated_values)
+    rmse = sse_to_rmse(sse, N_values)
+    return rmse
+
+@numba.njit(cache=True)
+def akaike_ic(maximum_loglikelihood, N_params):
+    """Akaike information criterion.
+    """
+    return 2 * N_params + 2 * maximum_loglikelihood
+
+@numba.njit(cache=True)
+def bayesian_ic(maximum_loglikelihood, N_params, N_data):
+    """Bayesian information criterion.
+    """
+    return np.log(N_data) * N_params - 2.* maximum_loglikelihood
