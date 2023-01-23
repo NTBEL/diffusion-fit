6552265944debe5695aa6f0490d0401f0d054302
blakeaw <blakeaw1102@gmail.com>
2023-01-23 16:18:30 -0600

    Modifications to include the AsymmetricFit fit class and fitting options.

--- diffusionfit/streamlit_app.py
+++ diffusionfit/streamlit_app.py
@@ -10,7 +10,9 @@ import plotly.graph_objects as go
 from tifffile import imwrite as tiffwrite
 
 # diffusionfit
-from diffusionfit import GaussianFit, PointClarkFit, AnisotropicGaussianFit
+from diffusionfit import GaussianFit, PointClarkFit
+from diffusionfit import AnisotropicGaussianFit
+from diffusionfit import AsymmetricFit
 
 # Cached convert from:
 # https://docs.streamlit.io/library/api-reference/widgets/st.download_button
@@ -24,12 +26,14 @@ model_options = {
     "Standard Gaussian": GaussianFit,
     "Anisotropic Gaussian": AnisotropicGaussianFit,
     "Point-Clark": PointClarkFit,
+    "Asymmetric": AsymmetricFit,
 }
 
 model_equations = {
     "Standard Gaussian": r"I = I_{\textrm{max}} e^{-\left(r / \gamma \right)^2}",
     "Anisotropic Gaussian": r"I = I_{\textrm{max}} e^{-\left[(x-x_0)/\gamma_x\right]^2} e^{-\left[(y-y_0)/\gamma_y \right]^2}",
     "Point-Clark": r"I = \frac{I_{\textrm{max}}}{1 + \beta e^{-\left(r / \gamma \right)^2}}",
+    "Asymmetric": r"I = I_{\textrm{max}} e^{-\left(r / \gamma \right)^2}, \, \frac{N_+}{N_-}=\sqrt{\frac{D^{*}_{+}}{D^{*}_{-}}}, \, D^*=(D^*_{+} + D^*_-)/2"
 }
 
 st.title("Diffusion Fitting App")
@@ -46,7 +50,7 @@ imgfile = st.file_uploader("Choose a fluorescence image file:", type=["tif", "ti
 # st.pyplot(fig)
 which_model = st.selectbox(
     "Choose Intensity Model:",
-    ("Standard Gaussian", "Anisotropic Gaussian", "Point-Clark"),
+    ("Standard Gaussian", "Anisotropic Gaussian", "Point-Clark", "Asymmetric"),
 )
 model = model_options[which_model]
 st.latex(model_equations[which_model])
@@ -54,7 +58,7 @@ st.markdown("------")
 
 required_input_columns = st.columns(4)
 
-timestep = required_input_columns[0].number_input("time step (s) :", 0.1, value=0.25)
+timestep = required_input_columns[0].number_input("time step (s) :", 0.01, value=0.25)
 pixel_size = required_input_columns[1].number_input("pixel size (um):", 0.1, value=1.0)
 stim_frame = required_input_columns[2].number_input("stimulation frame:", 1)
 d_stim = required_input_columns[3].number_input("stim. zone diameter (um):", 0.0)
@@ -67,6 +71,12 @@ col1, col2 = st.columns(2)
 apply_threshold = col1.checkbox("Apply threshold", value=True)
 threshold_value = col2.number_input("Threshold value (signal/noise):", 1.0, 10.0, 3.0)
 
+if which_model == 'Asymmetric':
+    st.markdown("------")
+    st.write("Asymmetric diffusion parameters")
+    asymm_input_cols = st.columns(2)
+    free_diffusion_coeff = asymm_input_cols[0].number_input("Free diffusion (x10^-7 cm2/s): ", 0.01, 100., 0.01)
+    asymm_axis = asymm_input_cols[1].radio("Axis of asymmetry: ", ['x', 'y'])
 st.markdown("------")
 
 # def fit_the_model():
@@ -83,32 +93,61 @@ if st.button("Fit the model"):
     if imgfile is not None:
         #     st.write("Fitting: ", imgfile.name)
         with st.spinner("Fitting..."):
-            dfit = model(
-                imgfile,
-                stimulation_frame=stim_frame,
-                timestep=timestep,
-                pixel_width=pixel_size,
-                stimulation_radius=d_stim / 2,
-                center=center,
-                subtract_background=subtract_background,
-            )
+            if which_model == 'Asymmetric':
+                dfit = model(
+                    imgfile,
+                    stimulation_frame=stim_frame,
+                    timestep=timestep,
+                    pixel_width=pixel_size,
+                    stimulation_radius=d_stim / 2,
+                    center=center,
+                    subtract_background=subtract_background,
+                    asymm_axis = asymm_axis,
+                )
+            else:    
+                dfit = model(
+                    imgfile,
+                    stimulation_frame=stim_frame,
+                    timestep=timestep,
+                    pixel_width=pixel_size,
+                    stimulation_radius=d_stim / 2,
+                    center=center,
+                    subtract_background=subtract_background,
+                )
             st.write(imgfile.name, " has ", dfit.n_frames, " frames")
-            D = dfit.fit(
-                apply_step1_threshold=apply_threshold, step1_threshold=threshold_value
-            )
+            if which_model == 'Aymmetric':
+                D = dfit.fit(
+                    apply_step1_threshold=apply_threshold, step1_threshold=threshold_value,
+                    free_diffusion = (free_diffusion_coeff * 1e-7),
+                )
+            else:    
+                D = dfit.fit(
+                    apply_step1_threshold=apply_threshold, step1_threshold=threshold_value
+                )
             st.session_state.fit_state = True
             st.session_state.fitted_model = dfit
+            st.session_state.diffco = D
     else:
         st.error("No image file selected!")
 st.markdown("------")
 
 if st.session_state.fit_state:
     dfit = st.session_state.fitted_model
-    D = dfit._Ds
+    D = st.session_state.diffco
     st.text("\n\n")
-    st.write(
-        "Estimated diffusion coefficient ", r"(x10^{-7} cm^2/s): ", np.round(D * 1e7, 2)
-    )
+    if which_model == "Anisotropic Gaussian":
+        st.write(
+            "Estimated Anisotropic diffusion coefficients ", r"(x10^{-7} cm^2/s): ", np.round(D * 1e7, 2)
+        )        
+    elif which_model == "Asymmetric":
+        st.write(
+            "Estimated Asymmetric diffusion coefficients ", r"(x10^{-7} cm^2/s): ", np.round(D * 1e7, 2)
+        )          
+    else:        
+        st.write(
+            "Estimated diffusion coefficient ", r"(x10^{-7} cm^2/s): ", np.round(D * 1e7, 2)
+        )
+
     st.write("Effective Time (s) : ", np.round(dfit.effective_time, 2))
     nframes = len(dfit._idx_fitted_frames)
     # Downloads for fitting parameters/data.
